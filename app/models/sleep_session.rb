c80a5ca6bf2f76b1fe6868f811de6e6b91822821
edalbert <edalbert.escalante+eabe@gmail.com>
2023-03-06 14:58:07 +0800

    Create SleepSession model and run migration
    https://www.notion.so/SleepSession-Model-a2db009dc50146649a15aac76e313f0a?pvs=4

--- app/models/sleep_session.rb
+++ app/models/sleep_session.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: sleep_sessions
+#
+#  id         :integer          not null, primary key
+#  start_time :datetime
+#  end_time   :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class SleepSession < ApplicationRecord
 end
--- test/models/sleep_session_test.rb
+++ test/models/sleep_session_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: sleep_sessions
+#
+#  id         :integer          not null, primary key
+#  start_time :datetime
+#  end_time   :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class SleepSessionTest < ActiveSupport::TestCase
